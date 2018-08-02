0c0e951f31349708b3d53a326bf60d2a1567be74
Bastien Thomas <thomas.bastien@gmx.com>
2018-08-02 19:00:09 +0200

    Add link of user perm to nav

--- nav.php
+++ nav.php
@@ -1,6 +1,7 @@
 <nav>
     <li> <a href="index.php"> <i class="fa fa-home"> </i>  Accueil  </a> </li>
     <li> <a href="add_article.php"> <i class="fa fa-home"> </i>  Article  </a> </li>
+    <li> <a href="user_perm.php?id=1"> <i class="fa fa-home"> </i>  Perm User </a> </li>
     <li> <a href="group_perm.php?id=1"> <i class="fa fa-home"> </i>  Perm Group  </a> </li>
     <?php
         if (isset($_SESSION["id"])) {
